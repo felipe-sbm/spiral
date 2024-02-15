4e1163aebf7f0a5385c3d220b654320c12da128a
felipe-sbm <felipesbm2005@gmail.com>
2024-02-15 19:05:19 -0300

    Atualização básica

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
+    <div className="pointer-events-none flex h-screen flex-col items-center justify-center space-y-10 bg-black">
       <Image
         width={512}
         height={512}
         src="/logo.png"
         alt="Platforms on Vercel"
-        className="w-48"
+        className="w-48 animate-bounce"
       />
       <h1 className="text-white">
         Edit this page on{" "}
@@ -17,6 +17,7 @@ export default function HomePage() {
           app/home/page.tsx
         </InlineSnippet>
       </h1>
+
     </div>
   );
 }
