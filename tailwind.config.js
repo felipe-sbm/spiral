7541f080521648585339a9e2ba9844f14ae8a692
felipe-sbm <felipesbm2005@gmail.com>
2024-07-09 14:39:14 -0300

    Corrigindo estrutura

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./app/layout.tsx"],
   theme: {
-    colors: {
-      "roxye-3": "#270046",
-      "roxye-2": "#2C0050",
-      "roxye-1": "#32005A",
-      "roxye-0": "#380064",
-      "roxye--1": "#4B1973",
-      "roxye--2": "#5F3383",
-      "roxye--3": "#734C92",
+    extend: {
+      colors: {
+        "roxye-3": "#270046",
+        "roxye-2": "#2C0050",
+        "roxye-1": "#32005A",
+        "roxye-0": "#380064",
+        "roxye--1": "#4B1973",
+        "roxye--2": "#5F3383",
+        "roxye--3": "#734C92",
+      },
     },
   },
 };
