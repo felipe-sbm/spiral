e5df91ce159ffe0477c238e72afef86b75784336
felipe-sbm <felipesbm2005@gmail.com>
2024-07-20 14:22:41 -0300

    Header praticamente concluído

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import {
-  Home,
+  DoorOpen,
   Shell,
-  FlaskConical,
+  Newspaper,
   BookImage,
   MessageCircleHeart,
-  PhoneCall,
+  MailPlus,
   Menu,
   X,
 } from "lucide-react";
@@ -18,95 +16,142 @@ export function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
-          <div className="md:flex md:items-center md:gap-12">
-            <a className="block text-teal-600" href="#">
-              <span className="sr-only">Início</span>
-              <Image
-                src="/spiral.png"
-                alt="Spiral Logo"
-                width={132}
-                height={132}
-                className="invert dark:invert-0"
+    <header className="bg-white dark:bg-slate-950 w-screen">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Spiral SBM</span>
+            <img
+              className="h-14 w-auto invert hover:opacity-75 dark:invert-0"
+              src="/spiral.png"
+              alt="Logo da Spiral SBM"
+            />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-white"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Abrir menu principal</span>
+            <Menu />
+          </button>
+        </div>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <a
+            href="/"
+            className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            INÍCIO
+          </a>
+          <a
+            href="/categorias/lojas"
+            className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            A SPIRAL
+          </a>
+          <a
+            href="/categorias/encarte"
+            className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            NOSSOS PROJETOS
+          </a>
+          <a
+            href="/categorias/supermomentos"
+            className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            GALERIA
+          </a>
+          <a
+            href="/categorias/trabalhe"
+            className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            DOAÇÕES
+          </a>
+          <a
+            href="/categorias/trabalhe"
+            className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            CONTATO
+          </a>
+        </PopoverGroup>
+      </nav>
+      <Dialog
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-slate-950/10 dark:bg-slate-950">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Spiral</span>
+              <img
+                className="h-12 w-auto round invert hover:opacity-75 dark:invert-0"
+                src="/logo.png"
+                alt="Logo da Spiral SBM"
               />
             </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-white"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Fechar menu</span>
+              <X />
+            </button>
           </div>
-
-          <div className="hidden md:block">
-            <nav aria-label="Global">
-              <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-purple-950/75"
-                    href="/"
-                  >
-                    INÍCIO
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-purple-950/75"
-                    href="/pages/about"
-                  >
-                    A SPIRAL
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-purple-950/75"
-                    href="/pages/projects"
-                  >
-                    NOSSOS PROJETOS
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-purple-950/75"
-                    href="/pages/galery"
-                  >
-                    GALERIA
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-purple-950/75"
-                    href="/pages/supportus"
-                  >
-                    DOAÇÕES
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-
-          <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <a
-                className="rounded-md bg-purple-700 px-5 py-2.5 text-sm font-medium text-white shadow"
-                href="/pages/contact"
-              >
-                CONTATO
-              </a>
-            </div>
-
-            <div className="block md:hidden">
-              <button
-                className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-purple-950/75"
-                button-type="menu"
-                title="Menu"
-              >
-                <Menu />
-              </button>
+          <div className="mt-6 flow-root">
+            <div className="my-6 divide-y divide-gray-500/10">
+              <div className="space-y-5 py-6">
+                <a
+                  href="#"
+                  className="flex -mx-3 block indent-2 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+                >
+                  <DoorOpen /> Início
+                </a>
+                <a
+                  href="#"
+                  className="flex -mx-3 block indent-2 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+                >
+                  <Shell />A Spiral
+                </a>
+                <a
+                  href="#"
+                  className="flex -mx-3 block indent-2 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+                >
+                  <Newspaper />
+                  Nosso Projetos
+                </a>
+                <a
+                  href="#"
+                  className="flex -mx-3 block indent-2 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+                >
+                  <BookImage />
+                  Galeria
+                </a>
+                <a
+                  href="#"
+                  className="flex -mx-3 block indent-2 rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+                >
+                  <MessageCircleHeart />
+                  Doações
+                </a>
+                <a
+                  href="#"
+                  className="flex -mx-3 block indent-2 rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white"
+                >
+                  <MailPlus /> Contato
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </DialogPanel>
+      </Dialog>
     </header>
   );
 }
