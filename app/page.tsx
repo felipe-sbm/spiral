0139402b10a57dada135c2ea3b081bb71f3b2cf1
felipe-sbm <felipesbm2005@gmail.com>
2024-08-07 22:10:00 -0300

    Atualizar página removendo seção de medalhas

--- app/page.tsx
+++ app/page.tsx
@@ -27,22 +27,6 @@ export default function Page() {
             Venha fazer parte dessa jornada conosco!
           </strong>
         </div>
-
-        {/* Medalhas */}
-        <div className="mt-2 space-x-2">
-        <span className="inline-flex items-center justify-center rounded-full border dark:border-zinc-50 px-2.5 py-0.5 zinc-50">
-          <Triangle className="-ms-1 me-1.5 h-3 w-3 fill-zinc-950 dark:fill-zinc-50"/>
-          <p className="whitespace-nowrap text-sm">Vercel</p>
-        </span>
-        <span className="inline-flex items-center justify-center rounded-full border border-amber-500 px-2.5 py-0.5 text-amber-700">
-        <Zap className="-ms-1 me-1.5 h-4 w-4" />
-        <p className="whitespace-nowrap text-sm">Vite</p>
-        </span>
-        <span className="inline-flex items-center justify-center rounded-full border border-red-500 px-2.5 py-0.5 text-red-700">
-          <Gem className="-ms-1 me-1.5 h-4 w-4" />
-          <p className="whitespace-nowrap text-sm">Rails</p>
-        </span>
-        </div>
       </div>
     </div>
   );
